5fbe9a9bdc7e2947a3259fcb3a34c5a67af7c19d
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-09-26 21:44:06 +1300

    add: shape data (diamond, acorn)

---
+++ src/data/shapes.js
@@ -0,0 +1,15 @@
+export const shapes = {
+  diamond: {
+    name:"diamond",
+    description:"pure glider generator",
+    value:[
+    4, 5, 6, 7, 26, 27, 28, 29, 30, 31, 32, 33, 48, 49, 50, 51, 52, 53, 54, 55,
+    56, 57, 58, 59, 74, 75, 76, 77, 78, 79, 80, 81, 113, 114, 115, 116,
+  ]},
+  acorn:{
+    acorn:"acorn",
+    description:'(stabilizes at time 5206) A methuselah found by Charles Corderman. It has a final population of 633 and covers an area of 215 by 168 cells, not counting the 13 gliders it produces. Its ash consists of typical stable objects and blinkers, along with the relatively rare mango and a temporary eater1.',
+    value:[1,10,21,22,25,26,27]
+  },
+
+};
